f4cb243df6382704c26c89ceee34b54829e0e925
meidad raz <meidad.raz@gmail.com>
2018-11-11 12:37:11 +0200

    Chapter 4 -Graphics

---
+++ ch04 Graphics/GraphicsDemos/05 LowLevelRendering/Declarative.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LowLevelRendering
+{
+    public class Declarative : Grid
+    {
+        public Declarative()
+        {
+            Rectangle r = new Rectangle();
+            r.Fill = Brushes.Red;
+            Children.Add(r);
+            Ellipse e = new Ellipse()
+            {
+                Fill = Brushes.Yellow,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            Children.Add(e);
+        }
+    }
+}

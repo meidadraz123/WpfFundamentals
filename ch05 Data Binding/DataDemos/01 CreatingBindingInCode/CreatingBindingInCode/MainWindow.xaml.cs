fdb562d19f628f41416d9e6bd1a561d0e9d76fa8
meidad raz <meidad.raz@gmail.com>
2018-11-13 12:42:11 +0200

    Chapter 05 - Data Binding

---
+++ ch05 Data Binding/DataDemos/01 CreatingBindingInCode/CreatingBindingInCode/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace CreatingBindingInCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Equivalent to:
+            // Text = "{Binding Path=CurrentCulture, Source={x:Static t:Thread.CurrentThread}}"
+
+            var binding = new Binding(nameof(CultureInfo.CurrentCulture))
+            {
+                Source = Thread.CurrentThread
+            };
+            CsBindindData.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
